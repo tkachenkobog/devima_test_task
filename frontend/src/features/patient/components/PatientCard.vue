38fbfe6aa47de11a4af3cc4bd767442d6504429c
tkachenkobog <t.bogdansmm@gmail.com>
2025-10-27 09:56:17 +0200

    patient-card

--- frontend/src/features/patient/components/PatientCard.vue
+++ frontend/src/features/patient/components/PatientCard.vue
@@ -2,6 +2,7 @@
 import { ElCard } from "element-plus";
 import { onMounted } from "vue";
 
+import patientImage from "@/assets/patient.webp";
 import { usePatientStore } from "@/features/patient";
 
 const patientStore = usePatientStore();
@@ -13,16 +14,45 @@ onMounted(() => {
 
 <template>
   <ElCard class="card">
-    <div v-if="patientStore.patient">
-      <p>Name: {{ patientStore.patient.name }}</p>
-      <p>Age {{ patientStore.patient.age }}</p>
-      <p>Gender{{ patientStore.patient.gender }}</p>
+    <div class="img-box">
+      <img :src="patientImage" alt="Patient Image" class="patient-image" />
+    </div>
+    <div v-if="patientStore.patient" class="patient-data">
+      <p><strong>Name:</strong> {{ patientStore.patient.name }}</p>
+      <p><strong>Age:</strong> {{ patientStore.patient.age }}</p>
+      <p><strong>Gender:</strong> {{ patientStore.patient.gender }}</p>
     </div>
   </ElCard>
 </template>
 
 <style scoped>
 .card {
-  margin: 10px;
+  height: 100%;
+  max-width: 450px;
+}
+
+:deep(.el-card__body) {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.img-box {
+  flex-grow: 1;
+  display: flex;
+  max-height: 320px;
+  justify-content: center;
+  align-items: center;
+}
+
+.patient-image {
+  max-width: 100%;
+  max-height: 100%;
+  border-radius: 6px;
+}
+
+.patient-data {
+  text-align: center;
+  padding-top: 16px;
 }
 </style>
