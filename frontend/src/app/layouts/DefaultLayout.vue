51f2ebf0d556f619b83a2efce1ef3c2326012360
tkachenkobog <t.bogdansmm@gmail.com>
2025-10-27 10:00:58 +0200

    layout, menu, footer as a servise

--- frontend/src/app/layouts/DefaultLayout.vue
+++ frontend/src/app/layouts/DefaultLayout.vue
@@ -1,49 +1,78 @@
+<script setup lang="ts">
+import { ElContainer, ElHeader, ElMain, ElFooter, ElMenu, ElMenuItem, ElLink } from "element-plus";
+</script>
+
 <template>
-  <ElContainer class="common-layout">
+  <ElContainer class="layout">
     <ElHeader>
-      <ElMenu mode="horizontal" :router="true">
-        <ElMenuItem index="/"> Home </ElMenuItem>
-        <ElMenuItem index="/about"> About </ElMenuItem>
+      <ElMenu 
+        mode="horizontal" 
+        :router="true" 
+        default-active="/"
+        class="menu"
+      >
+        <ElMenuItem index="/">Home</ElMenuItem>
+        <ElMenuItem index="/about">About</ElMenuItem>
       </ElMenu>
     </ElHeader>
-    <ElMain>
+
+    <ElMain class="main-content">
       <router-view />
     </ElMain>
-    <ElFooter>Footer</ElFooter>
+
+    <ElFooter class="footer">
+      <div class="content">
+        <span>© 2025</span>
+        <ElLink 
+          href="https://www.linkedin.com/in/tkachenko-bohdan" 
+          target="_blank"
+          :underline="false"
+          type="primary"
+          class="f-link"
+        >
+          Bohdan Tkachenko.
+        </ElLink>
+        <span>Footer as a Service™</span>
+      </div>
+    </ElFooter>
   </ElContainer>
 </template>
 
-<script setup lang="ts">
-import { ElContainer, ElHeader, ElMain, ElFooter, ElMenu, ElMenuItem } from "element-plus";
-</script>
-
 <style scoped>
-.common-layout {
+.layout {
   min-height: 100vh;
+  display: flex;
+  flex-direction: column;
 }
 
-.el-header {
-  background-color: #b3c0d1;
-  color: var(--el-text-color-primary);
-  line-height: 60px;
-  padding: 0;
+.menu {
+  display: flex;
+  justify-content: center;
 }
 
-.el-menu {
-  border-bottom: none;
+.main-content {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
 }
 
-.el-footer {
-  background-color: #b3c0d1;
-  color: var(--el-text-color-primary);
-  text-align: center;
-  line-height: 60px;
-}
-
-.el-main {
-  background-color: #e9eef3;
-  color: var(--el-text-color-primary);
-  text-align: center;
+.footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   padding: 20px;
+  background-color: #f5f5f5;
+  border-top: 0.5px solid #e0e0e0;
+  margin-top: auto;
+}
+
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 6px;
+  color: #666;
+  font-size: 14px;
+  flex-wrap: wrap;
 }
 </style>
